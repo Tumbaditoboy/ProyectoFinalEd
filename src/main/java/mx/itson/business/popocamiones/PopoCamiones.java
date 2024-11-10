ec109b9358ff0800e7a8b0dffbbab524cc6451a9
Akane <>
2024-11-09 18:24:59 -0700

    Iniciación y prueba del proyecto

---
+++ src/main/java/mx/itson/business/popocamiones/PopoCamiones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.business.popocamiones;
+
+/**
+ *
+ * @author Akane
+ */
+public class PopoCamiones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
